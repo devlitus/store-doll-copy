6acd709a427a0016e5aad7d183acdf563ff08b20
devlitus <developercales@gmail.com>
2022-09-04 07:20:56 +0200

    Implement screen  home

--- lib/screens/homa_screen.dart
+++ lib/screens/homa_screen.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:store_dolls/widgets/card_home.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    final size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(children: [
+          Center(
+            child: CardHome(
+              size: size,
+              title: 'Amigurumi',
+              image: 'images/amigurumi.jpg',
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: CardHome(
+              size: size,
+              title: 'Ganchillo',
+              image: 'images/ganchillo.jpg',
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: CardHome(
+              size: size,
+              title: 'Dolls',
+              image: 'images/ganchillo.jpg',
+            ),
+          ),
+        ]),
       ),
     );
   }
